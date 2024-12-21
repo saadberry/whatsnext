4da89faf61cd3a9c5eb22e9d7ae135c775c9a6b0
saadberry <saadberry29@gmail.com>
2024-12-21 17:45:57 +0500

    Hashing password whilst user creation

--- server/src/app/services/userService.js
+++ server/src/app/services/userService.js
@@ -1,11 +1,15 @@
 /*
 User related services
 */
-const { constants } = require("../../constants");
+const bcrypt = require("bcrypt")
+
+const { saltRounds } = require("../../constants");
 const User = require("../models/users")
 
 exports.create = async (name, email, password) => {
-    const new_user = new User({name, email, password})
+    hashed_password = await bcrypt.hash(password, saltRounds)
+    console.log(`hashed_password=${JSON.stringify(hashed_password)}`)
+    const new_user = new User({name, email, password:hashed_password})
     return await new_user.save();
 }
 
