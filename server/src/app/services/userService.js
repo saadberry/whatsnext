5343106322d4c488defe508a8a6ebec06c22a246
saadberry <saadberry29@gmail.com>
2024-12-24 01:00:55 +0500

    Removing log statements

--- server/src/app/services/userService.js
+++ server/src/app/services/userService.js
@@ -7,10 +7,10 @@ const { saltRounds } = require("../../constants");
 const User = require("../models/users")
 const userUtils = require("../utils/userUtils")
 const userValidators = require("../validators/userValidators.ts")
+const jwt = require("jsonwebtoken")
 
 exports.create = async (name, email, password) => {
     hashed_password = await userUtils.hashPassword(password)
-    console.log(`hashed_password=${JSON.stringify(hashed_password)}`)
     const new_user = new User({name, email, password:hashed_password})
     return await new_user.save();
 }
@@ -21,7 +21,12 @@ exports.login = async (email, password) => {
     // Compare passwords
     const is_valid_password = await userValidators.validatePassword(password, user["password"])
     if (is_valid_password) {
-        return user
+        const token = jwt.sign(
+            { userId: user._id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        );
+        return {user, token};
     } else {
         return null
     }
