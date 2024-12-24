c4e9e15a1b7ce2fb4250afc3e6860a9524b32cff
saadberry <saadberry29@gmail.com>
2024-12-24 11:18:24 +0500

    Removing log statements

--- server/src/connect.js
+++ server/src/connect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 const MONGO_URI = process.env.MONGO_URI;
-console.log(`MONGO_URI=${MONGO_URI}`)
 
 async function connectToDatabase() {
     try {
